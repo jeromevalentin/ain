65602b7cfadbcb0704741eb8df55661b90108919
Jerome Valentin <jerome.valentin@scantech.fr>
2017-07-12 19:14:05 +0200

    Fix #43 - Use local time instead of UTC time in events
    implementation adapted from https://stackoverflow.com/questions/17415579/how-to-iso-8601-format-a-date-with-timezone-offset-in-javascript

--- index.js
+++ index.js
@@ -442,7 +442,21 @@ SysLogger.prototype.timeEnd = function(label) {
  * @returns {String}
  */
 SysLogger.prototype.getDate = function() {
-    return new Date().toISOString();
+    var date = new Date()
+    var tzo = -date.getTimezoneOffset(),
+        dif = tzo >= 0 ? '+' : '-',
+        pad = function(num) {
+            var norm = Math.abs(Math.floor(num));
+            return (norm < 10 ? '0' : '') + norm;
+        };
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds()) +
+        dif + pad(tzo / 60) +
+        ':' + pad(tzo % 60);
 }
 
 SysLogger.prototype.leadZero = function(n) {
